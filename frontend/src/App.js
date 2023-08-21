5dcdbc1418638e91cf42275c5facf4ec2361cccc
Baek <woonn97@gmail.com>
2023-08-21 18:21:59 +0900

    install react.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        안녕
+      </header>
+    </div>
+  );
+}
+
+export default App;
