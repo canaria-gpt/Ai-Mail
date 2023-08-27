21191bcc139742203fbd9326982d764f87d201a6
sso9594 <sso9594@naver.com>
2023-08-27 16:00:52 +0900

    feat(Auth_server): featuring error response

--- src/main/java/com/canaria/Auth_server/controller/JwtLoginApiController.java
+++ src/main/java/com/canaria/Auth_server/controller/JwtLoginApiController.java
@@ -5,6 +5,7 @@ import com.canaria.Auth_server.dto.JoinRequest;
 import com.canaria.Auth_server.dto.LoginRequest;
 import com.canaria.Auth_server.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -27,23 +28,28 @@ public class JwtLoginApiController {
 
      */
     @PostMapping("/join")
-    public String join(@RequestBody JoinRequest joinRequest) {
-
+    public ResponseEntity<String> join(@RequestBody JoinRequest joinRequest) {
         // loginId 중복 체크
         if(userService.checkLoginIdDuplicate(joinRequest.getLoginId())) {
-            return "로그인 아이디가 중복됩니다.";
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("회원가입 실패");
         }
         // 닉네임 중복 체크
         if(userService.checkNicknameDuplicate(joinRequest.getNickname())) {
-            return "닉네임이 중복됩니다.";
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("회원가입 실패");
         }
         // password와 passwordCheck가 같은지 체크
         if(!joinRequest.getPassword().equals(joinRequest.getPasswordCheck())) {
-            return"바밀번호가 일치하지 않습니다.";
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("회원가입 실패");
         }
 
         userService.join2(joinRequest);
-        return "회원가입 성공";
+        return ResponseEntity.ok("회원가입 성공");
     }
 
     @PostMapping("/login")
@@ -53,15 +59,6 @@ public class JwtLoginApiController {
 
     }
 
-    /*
-    @GetMapping("/info")
-    public String userInfo(Authentication auth) {
-        User loginUser = userService.getLoginUserByLoginId(auth.getName());
-
-        return String.format("loginId : %s\nnickname : %s\nrole : %s",
-                loginUser.getLoginId(), loginUser.getNickname(), loginUser.getRole().name());
-    }
-    */
     @GetMapping("/admin")
     public String adminPage() {
         return "관리자 페이지 접근 성공";
